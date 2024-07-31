6c19861bf5a7f4da2e4590103da8dfb301408cc7
OliverSchacht <65898638+OliverSchacht@users.noreply.github.com>
2024-07-31 16:09:14 +0200

    add support to "IV-type" in PLR

--- autodml/AutoDoubleMLPLR.py
+++ autodml/AutoDoubleMLPLR.py
@@ -1,7 +1,8 @@
 from flaml import AutoML
 from doubleml import DoubleMLPLR
 from sklearn.dummy import DummyRegressor
-import warnings
+import numpy as np
+from sklearn.model_selection import cross_val_predict
 
 from autodml._utils import assert_time
 
@@ -17,6 +18,7 @@ class AutoDoubleMLPLR(DoubleMLPLR):
         super().__init__(obj_dml_data,
                          ml_m=DummyRegressor(),
                          ml_l=DummyRegressor(),
+                         ml_g=DummyRegressor() if score=="IV-type" else None,
                          n_folds=n_folds,
                          n_rep=n_rep,
                          score=score,
@@ -31,10 +33,9 @@ class AutoDoubleMLPLR(DoubleMLPLR):
         if not framework in ["flaml"]:
             raise ValueError(f'Currently only framework "flaml" is supported \
                               but {framework} was provided')
-        if score=="IV-type":
-            raise NotImplementedError('Currently only "partialling out" is supported') 
-
+        
         self.task_m = "classification" if self._dml_data.binary_treats.all() else "regression"
+
         if framework == "flaml":
             self.automl_l = AutoML(time_budget=self.time['ml_l'],
                                    metric='rmse',  
@@ -42,16 +43,41 @@ class AutoDoubleMLPLR(DoubleMLPLR):
             self.automl_m = AutoML(time_budget=self.time['ml_m'],
                                    metric='rmse',  
                                    task=self.task_m)
+            if score=="IV-type":
+                self.automl_g = AutoML(time_budget=self.time['ml_g'],
+                                    metric='rmse',  
+                                    task='regression')
 
-    def fit(self):
-        print(f"Optimizing learners for {self.time}s. Please wait.")
-        self.automl_l.fit(X_train=self._dml_data.x, 
-                            y_train=self._dml_data.y, verbose=0)
-        self.automl_m.fit(X_train=self._dml_data.x, 
-                            y_train=self._dml_data.d, verbose=0)
+    def fit(self, n_jobs_cv=None, store_models=False):
+        learners_info = ', '.join([f"{learner} for {duration}s" for learner, duration in self.time.items()])
+        print(f"Optimizing learners: {learners_info}. Please wait.")
+
+        x, y, d = self._dml_data.x, self._dml_data.y, self._dml_data.d
+        
+        self.automl_l.fit(X_train=x, 
+                            y_train=y, verbose=0)
+        self.automl_m.fit(X_train=x, 
+                            y_train=d, verbose=0)
         self._learner = {'ml_l': self.automl_l.model.estimator, 
-                            'ml_m': self.automl_m.model.estimator}
+                         'ml_m': self.automl_m.model.estimator}
+        
         if self.task_m == "classification":
             self._predict_method['ml_m'] = 'predict_proba'
-        _fit  = super().fit()
+        
+        if self._score == "IV-type":
+            m_hat = cross_val_predict(self.automl_m.model.estimator, x, d,
+                                      method=self._predict_method['ml_m'], cv=self.n_folds, n_jobs=n_jobs_cv)
+            l_hat = cross_val_predict(self.automl_l.model.estimator, x, y,
+                                      cv=self.n_folds, n_jobs=n_jobs_cv)
+            
+            psi_a = -np.multiply(d - m_hat, d - m_hat)
+            psi_b = np.multiply(d - m_hat, y - l_hat)
+            theta_initial = -np.nanmean(psi_b) / np.nanmean(psi_a)
+
+            self.automl_g.fit(X_train=x, 
+                              y_train=y - theta_initial*d, verbose=0)
+            self._learner["ml_g"] = self.automl_g.model.estimator
+
+        _fit  = super().fit(n_jobs_cv=n_jobs_cv, 
+                            store_models=store_models)
         return _fit
